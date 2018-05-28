80090a8b312bc152b7c442637f9603809a7cfd89
martineng <martin.eca.6651@gmail.com>
2018-05-28 06:50:15 -0700

    Completed Version 1.0

--- LAMPscript.sh
+++ LAMPscript.sh
@@ -8,7 +8,7 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 
 # Installing Apache
 echo -e "Getting Apache2..."
-sudo apt-get install apache2
+sudo apt-get install apache2 -y
 
 # Adjusting Firewall to Allow Web Traffic
 echo -e "Configuring Firewall..."
@@ -16,6 +16,17 @@ sudo ufw allow in "Apache Full"
 
 # Installing MySQL
 echo -e "Installing MySQL..."
-sudo apt-get install mysql-server
+sudo apt-get install mysql-server -y
 echo -e "Executing MySQL Secure Installation..."
 sudo mysql_secure_installation
+
+# Installing PHP
+echo -e "Installing PHP..."
+sudo apt-get install php libapache2-mod-php php-mysql -y
+
+# Installilng phpMyAdmin
+echo -e "Installing phpMyAdmin..."
+sudo apt-get install phpmyadmin php-mbstring php-gettext -y
+sudo phpenmod mbstring
+
+sudo systemctl restart apache2
